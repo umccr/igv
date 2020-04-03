4f996861f7713e875b4154a3d0f91993653bda54
Roman Valls Guimera <brainstorm@nopcode.org>
2020-04-03 14:47:52 +1100

    Mapping out CFN/CDK resources needed to be able to deploy IGV authentication backend on AWS (for both IGV desktop and IGV.js)

--- src/main/java/org/umccr/awscdk/IGVAmazonCognitoStack.java
+++ src/main/java/org/umccr/awscdk/IGVAmazonCognitoStack.java
@@ -8,24 +8,28 @@ import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
 
 import software.amazon.awscdk.services.cognito.UserPool;
+import software.amazon.awscdk.services.cognito.CfnUserPoolIdentityProvider;
 import software.amazon.awscdk.services.cognito.UserPoolClient;
-
 import software.amazon.awscdk.services.cognito.CfnIdentityPool;
-
 import software.amazon.awscdk.services.s3.Bucket;
+
+import java.util.List;
 
 public class IGVAmazonCognitoStack extends Stack {
     String awsRegion = this.getRegion();
     String providedS3Bucket;
 
+    // General Cognito attributes
+    String cognitoRoleARN;
+    String cognitoEndpointName;
+    // UserPool attributes
     String userPoolClientID;
     String userPoolClientSecret;
     String userPoolID;
     String userPoolARN;
-
-    String cognitoRoleARN;
+    // IdentityPool attributes
     String identityPoolName;
-    String cognitoEndpointName;
+
 
     public IGVAmazonCognitoStack(final Construct parent, final String id) {
         this(parent, id, null);
@@ -37,27 +41,47 @@ public class IGVAmazonCognitoStack extends Stack {
         providedS3Bucket = this.getNode().tryGetContext("s3bucket").toString();
         System.out.println(Bucket.fromBucketName(this, "IgvS3BucketBYO", providedS3Bucket).getBucketName());
 
-        // 4. Set Allowed OAuth scopes to email and profile?
-        // N. Show the admin which URL or oauth-config.json.gz to deploy
+        // TODO:
+        // Set Allowed OAuth scopes to: "email and profile". CFN==NotFound
+        // Set MFA and verification option to: "None – users will have to contact an administrator to reset their passwords". CFN==NotFound
+        // Set advanced security to "Audit-only", at least. CFN=="UserPoolAddOns": https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpool-userpooladdons.html. CDK==NotFound
+        // "Do you want to remember your user's devices?": Always. CFN==NotFound.
+        // "Prevent User Existence Errors": Enabled. CFN==NotFound.
 
-        final UserPool userPool = UserPool.Builder.create(this, "IGV User Pool").build();
-        final UserPoolClient userPoolClient = UserPoolClient.Builder.create(this, "IGV User Pool Client").userPool(userPool).build();
+        final UserPool userPool = UserPool.Builder.create(this, "IGV User Pool")
+                                                  .selfSignUpEnabled(false)
+
+                                                  .build();
+        final UserPoolClient userPoolClient = UserPoolClient.Builder.create(this, "IGV User Pool Client")
+                                                                    .userPool(userPool)
+                                                                    .userPoolClientName("IGV User Pool Client")
+                                                                    .generateSecret(true)
+                                                                    .build();
+
 
         userPoolClientID = userPoolClient.getUserPoolClientId();
         userPoolClientSecret = userPoolClient.getUserPoolClientClientSecret();
         userPoolID = userPool.getUserPoolId();
         userPoolARN = userPool.getUserPoolArn();
 
+        // TODO: Choose Google for our IDP of choice at UMCCR... but we might want to parametrize/generalize this
+        final CfnUserPoolIdentityProvider userPoolIDP = CfnUserPoolIdentityProvider.Builder.create(this, "IGV User Pool IDP")
+                                                                                           .userPoolId(userPoolID)
+                                                                                            .idpIdentifiers(List.of("Google"))
+                                                                                        .providerName("Google")
+                                                                                        .providerType("Google")
+                                                                                        .build();
+
+
         // XXX: UserPool created with ID: ${Token[TOKEN.25]} and ARN: ${Token[TOKEN.22]}
         System.out.println("UserPool created with ID: "+userPoolID+" and ARN: "+userPoolARN);
 
         final CfnIdentityPool identityPool = CfnIdentityPool.Builder.create(this, "IGV Identity Pool")
+                                                                    // Sadly, this is actually required to deploy the stack :-!!!
                                                                     .allowUnauthenticatedIdentities(true)
                                                                     .build();
 
         identityPoolName = identityPool.getIdentityPoolName();
-        //identityPool.setCognitoIdentityProviders();
-
 
         generateClientOauthConfig(userPoolClientID, userPoolClientSecret,
                                   identityPoolName, userPoolID, cognitoRoleARN,
