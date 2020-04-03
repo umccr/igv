dc0da56dbd266fd60a23e09824f84d913e6e3bab
Roman Valls Guimera <brainstorm@nopcode.org>
2020-04-03 16:07:52 +1100

    Add domain input string. Be more explicit about what is thought to be lacking were on Cloudformation and CDK /cc @reisingerf @victorskl @alexiswl

--- src/main/java/org/umccr/awscdk/IGVAmazonCognitoStack.java
+++ src/main/java/org/umccr/awscdk/IGVAmazonCognitoStack.java
@@ -12,6 +12,7 @@ import software.amazon.awscdk.services.cognito.UserPool;
 import software.amazon.awscdk.services.cognito.CfnUserPoolIdentityProvider;
 import software.amazon.awscdk.services.cognito.UserPoolClient;
 import software.amazon.awscdk.services.cognito.CfnIdentityPool;
+import software.amazon.awscdk.services.cognito.CfnUserPoolDomain;
 import software.amazon.awscdk.services.s3.Bucket;
 
 import java.util.HashMap;
@@ -22,6 +23,7 @@ public class IGVAmazonCognitoStack extends Stack {
     String providedS3Bucket;
 
     // General Cognito attributes
+    String userPoolDomain="igvdomainnameneedsparametrization";
     String cognitoRoleARN;
     String cognitoEndpointName;
     // UserPool attributes
@@ -43,16 +45,23 @@ public class IGVAmazonCognitoStack extends Stack {
         providedS3Bucket = this.getNode().tryGetContext("s3bucket").toString();
         System.out.println(Bucket.fromBucketName(this, "IgvS3BucketBYO", providedS3Bucket).getBucketName());
 
-        // TODO:
-        // Set Allowed OAuth scopes to: "email and profile". CFN==NotFound
-        // Set MFA and verification option to: "None – users will have to contact an administrator to reset their passwords". CFN==NotFound
-        // Set advanced security to "Audit-only", at least. CFN=="UserPoolAddOns": https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpool-userpooladdons.html. CDK==NotFound
-        // "Do you want to remember your user's devices?": Always. CFN==NotFound.
-        // "Prevent User Existence Errors": Enabled. CFN==NotFound.
+        // TODO: Those settings are in urgent need of programmatic modification (bad defaults). Most do not seem to be exposed as CloudFormation (n)or CDK constructs.
+        //
+        // General settings -> MFA and verifications:
+        //                                          -> "How will a user be able to recover their account?": "None – users will have to contact an administrator to reset their passwords". CFN==NotFound
+        //                                          -> Set MFA and verification option to: "None – users will have to contact an administrator to reset their passwords". CFN==NotFound
+        //                  -> Advanced Security:
+        //                                          -> "Do you want to enable advanced security features for this user pool?": "Audit Only". CFN=="UserPoolAddOns", CDK==NotFound?
+        //                  -> Devices:
+        //                                          -> "Do you want to remember your user's devices?": Always. CFN==NotFound, CDK==NotFound
+        // App Integration -> App client settings:
+        //                                          -> "Enabled Identity Providers": Check "Google" box. CFN==NotFound
+        //                                          -> "Allowed OAuth Flows": "Authorization code grant" only. CDK==NotFound
+        //                                          -> "Allowed OAuth scopes": email and profile only. CFN==NotFound
+        //                                          -> "Prevent User Existence Errors": Enabled. CFN==NotFound.
 
         final UserPool userPool = UserPool.Builder.create(this, "IGV User Pool")
                                                   .selfSignUpEnabled(false)
-
                                                   .build();
         final UserPoolClient userPoolClient = UserPoolClient.Builder.create(this, "IGV User Pool Client")
                                                                     .userPool(userPool)
@@ -65,6 +74,11 @@ public class IGVAmazonCognitoStack extends Stack {
         userPoolClientSecret = userPoolClient.getUserPoolClientClientSecret();
         userPoolID = userPool.getUserPoolId();
         userPoolARN = userPool.getUserPoolArn();
+
+        final CfnUserPoolDomain cfnUserPoolDomain = CfnUserPoolDomain.Builder.create(this, "IGV User Pool Domain")
+                .domain(userPoolDomain)
+                .userPoolId(userPoolID)
+                .build();
 
         // TODO: Choose Google for our IDP of choice at UMCCR... but we might want to parametrize/generalize this
         JsonObject extIDPCfg = externalIDPConfig();
